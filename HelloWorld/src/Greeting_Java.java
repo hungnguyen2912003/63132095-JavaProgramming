39008bb4a21394ce837772bc001de375dc48e2e0
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-01-14 18:40:20 +0700

    Example
    Làm quen với phần mềm Eclipse và Ngôn ngữ lập trình JAVA

---
+++ HelloWorld/src/Greeting_Java.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Greeting_Java {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào! Tôi đang làm quen ngôn ngữ lập trình JAVA trên phần mềm Eclipse");
+		System.out.println("Hello World");
+	}
+}

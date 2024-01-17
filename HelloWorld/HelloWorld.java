17ec6e383ad804f10054f3ba5b72bf380ac06544
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-01-17 21:54:10 +0700

    Update

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+package HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Xin chào! Tôi đang làm quen với Java trên IJ");
+    }
+}
--- HelloWorld/src/Greeting_Java.java
+++
@@ -1,9 +0,0 @@
-
-public class Greeting_Java {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Xin chào! Tôi đang làm quen ngôn ngữ lập trình JAVA trên phần mềm Eclipse");
-		System.out.println("Hello World");
-	}
-}

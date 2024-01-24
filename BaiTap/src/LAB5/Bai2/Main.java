3bfbc407c5c1a3c12a4fe7b3b69ca00537347add
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-01-24 17:03:33 +0700

    Init

---
+++ BaiTap/src/LAB5/Bai2/Main.java
@@ -0,0 +1,7 @@
+package LAB5.Bai2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- LAB/src/LAB5/Lab5_Bai2.java
+++
@@ -1,7 +0,0 @@
-package LAB5;
-
-public class Lab5_Bai2 {
-    public static void main(String[] args) {
-        
-    }
-}
